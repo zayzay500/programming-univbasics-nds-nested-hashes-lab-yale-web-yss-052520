481ae9fe30b59d855c8ce7db4dda93ed47444913
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-20 23:14:06 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,7 +17,10 @@ def literal_aoh
   # held in the constants
   [
     {name: "Don Gately", occupation: "Live-in Staff"},
-    {name: "Joelle van Dyne", occupation: "Radio Personality"}
+    {name: "Joelle van Dyne", occupation: "Radio Personality"},
+    {name: "Pat Monteseian", occupation: "Staff"},
+    {name: "Kate Gompert", occupation: "None"},
+    {name: "Bruce Green", occupation: "Fan of Mildred"}
   ]
 end
 
