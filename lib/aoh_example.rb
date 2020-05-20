42933df72a977e7f8c62d6c3f9285555b80ae1f4
Lindsay Pierce <lindsay.pierce@yale.edu>
2020-05-20 23:12:12 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,6 +15,10 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
+  [
+    {name: "Don Gately", occupation: "Live-in Staff"},
+    {name: "Joelle van Dyne", occupation: "Radio Personality"}
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
